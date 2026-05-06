939e166dc5f79a1e5e0df973829311cc3879bf35
jaerey1016@gmail.com <jaerey1016@gmail.com>
2026-05-06 22:05:24 +0000

    Edited hub_kiss

--- client_code/hub_kiss/__init__.py
+++ client_code/hub_kiss/__init__.py
@@ -13,21 +13,11 @@ class hub_kiss(hub_kissTemplate):
     self.init_components(**properties)
 
 
-
-  
-
-
-
   
     # Any code you write here will run before the form opens.
 
-  @handle("Home", "click")
-  def Home_click(self, **event_args):
-    """This event is called when the button is clicked"""    
-    open_form('hub_kiss')#go back to hub
-
   @handle("New_post_button", "click")
   def New_post_button_click(self, **event_args):
-    open_form('card')
+    open_form('card') #bring the user to the card adding page
     
     
