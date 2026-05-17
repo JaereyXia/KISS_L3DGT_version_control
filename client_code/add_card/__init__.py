ddbaa6227b94a2bc701c22b3d6c1cb43dd635f8d
jaerey1016@gmail.com <jaerey1016@gmail.com>
2026-05-17 07:35:09 +0000

    Created LICENSE.txt; Edited add_card

--- client_code/add_card/__init__.py
+++ client_code/add_card/__init__.py
@@ -13,9 +13,12 @@ class add_card(add_cardTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
     # Any code you write here will run before the form opens.
-
+    
+    '''If the key is true, it allow the post to be send, but if the key is false, 
+    it means the user haven't fill in all the context'''
+    self.check_key = False
+    
   @handle("back_home_botton", "click")
   def back_home_botton_click(self, **event_args):
     open_form('hub_kiss')
@@ -23,7 +26,15 @@ class add_card(add_cardTemplate):
   @handle("cancel_button", "click")
   def cancer_button_click(self, **event_args):
     """This method is called when the button is clicked"""
-    open_form('hub_kiss')
+    #Add double-checking to ensure users clearly understand that they want to cancel posting.
+    save_clicked = alert(
+      content="Are you sure to cancel this post?",
+      title="Cancel the post",
+      large=True,
+      buttons=[("Yes", True), ("No", False)],
+    )
+    if save_clicked:#if the user is sure and clicked Yes buttom, send him/her back to hub page
+      open_form('hub_kiss')
 
   @handle("save_button", "click")
   def save_button_click(self, **event_args):
@@ -31,15 +42,22 @@ class add_card(add_cardTemplate):
     post_title = self.post_title_descriptive # Set 'post_title' to the text in the 'self.post_title_descriptive'
     text = self.text_area_1 # Set 'text' to the text in the 'text area'
     # pass in post_title, text as arguments
-    self.check_blank_feedback_form()# check if the text area or post title is blank
+    self.check_blank_post()# check if the text area or post title is blank
     if self.key:#if the key is true, then it means that the user fill in the post title and post content
       anvil.server.call('add_feedback', post_title, text) # Set 'feedback' to the text in the 'feedback_box'
       Notification("Post created").show() # Show a popup that says 'Feedback submitted!'
 
-
-  
     self.clear_inputs() # Call your 'clear_inputs' method to clear the boxes
 
+  
+  def check_blank_post(self): # this is to check if the poster is fill in or not
+    #if any of the text box is blank, the code will send a notification to the user to tell him/her
+    if self.post_title_descriptive == "":
+      Notification("You haven't added a title to this article yet. Please check the title again.").show()
+    elif self.text_area_1 == "":
+      Notification("You haven't added a content to this article yet. Please check the content again.").show()
+    else:#else the code will tell the key that the user has fill the post and it's ready to be send.
+      self.check_key = True
     
     
 
