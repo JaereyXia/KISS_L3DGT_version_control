6427619ef9ad1ad8299e5bb27441f0250d9f1516
jaerey1016@gmail.com <jaerey1016@gmail.com>
2026-05-18 09:56:45 +0000

    Edited card_template

--- client_code/card_template/__init__.py
+++ client_code/card_template/__init__.py
@@ -15,3 +15,8 @@ class card_template(card_templateTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  @handle("edit_button", "click")
+  def edit_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    
