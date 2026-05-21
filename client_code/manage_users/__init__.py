1d1936d87e9b213c4075e196f773de969a2716a0
jaerey1016@gmail.com <jaerey1016@gmail.com>
2026-05-21 10:48:32 +0000

    Created user_card; Edited manage_users

--- client_code/manage_users/__init__.py
+++ client_code/manage_users/__init__.py
@@ -1,16 +1,45 @@
 from ._anvil_designer import manage_usersTemplate
 from anvil import *
 import anvil.server
-import anvil.google.auth, anvil.google.drive
-from anvil.google.drive import app_files
-import anvil.tables as tables
-import anvil.tables.query as q
-from anvil.tables import app_tables
-import anvil.users
 
 
-class manage_users(manage_usersTemplate):
-  def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
-    self.init_components(**properties)
-   
+class manage_users(
+  manage_usersTemplate
+):
+
+  def __init__(
+    self,
+    **properties
+  ):
+
+    self.init_components(
+      **properties
+    )
+
+    # Load users
+    self.refresh_users()
+
+
+  def refresh_users(self):
+
+    # Get all users
+    users = anvil.server.call(
+      'get_all_users'
+    )
+
+    # Show users
+    self.users_panel.items = users
+
+
+  @handle(
+    "back_button",
+    "click"
+  )
+  def back_button_click(
+    self,
+    **event_args
+  ):
+
+    open_form(
+      'hub_kiss'
+    )
